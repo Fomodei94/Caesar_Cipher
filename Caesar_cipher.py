e2094fd180b5d0f37c9cbafced36db2c0471733c
Federico <federico.omodei@gmail.com>
2020-05-28 00:26:04 +0200

    add grid layout, style and control over key values

--- Caesar_cipher.py
+++ Caesar_cipher.py
@@ -2,7 +2,7 @@
 # Author: Fomodei94
 # Python, Tk gui library
 # Date: 2013-10-19
-# Last Update: 2020-05-24
+# Last Update: 2020-05-27
 
 from random import randint
 from tkinter import *
@@ -32,19 +32,31 @@ def decrypt():
    plain_text.delete(1.0, END)
    plain_text.insert(1.0, decrypted_text)
 
+def limit_key_size(*args):
+  key_value = key.get()
+  if len(key_value) > 1: keyValue.set(key_value[1])
+
 app = Tk()
 app.title("Caesar Cipher by Fomodei94")
-app.geometry("600x800")
-Label(app, text = "Text to be encrypted:").pack(pady = 10)
+app.geometry("865x320")
+
+Label(app, text = "Plain text:", font = "default 11 bold").grid(row = 0, column = 0, pady = 10)
 plain_text = Text(app)
-plain_text.pack(padx = 30)
-Button(app, text = "Custom key encryption", command = custom_key_encrypt).pack(padx = 5, pady = 10)
-Button(app, text = "Random key encryption", command = random_key_encrypt).pack(anchor = "center")
-Label(app, text = "encrypt/decrypt key:").pack(pady = 7)
-key = Entry(app, width = 5, font = "default 13 bold", fg = "darkgreen", justify = "center")
-key.pack()
-Button(app, text = "Decrypt", command = decrypt).pack(anchor = "center", pady = 7)
-Label(app, text = "Encrypted text:").pack(pady = 5)
+plain_text.config(width = 40, height = 16)
+plain_text.grid(row = 1, column = 0, rowspan = 5, padx = 30)
+
+Button(app, text = "Custom key encryption", command = custom_key_encrypt).grid(row = 1, column = 1)
+Button(app, text = "Random key encryption", command = random_key_encrypt).grid(row = 2, column = 1, pady = 15)
+Label(app, text = "encrypt/decrypt key:").grid(row = 3, column= 1)
+keyValue = StringVar()
+keyValue.trace('w', limit_key_size)
+key = Entry(app, width = 5, font = "default 13 bold", fg = "green", justify = "center", textvariable = keyValue)
+key.grid(row = 4, column = 1)
+Button(app, text = "Decrypt", command = decrypt).grid(row = 5, column = 1, pady = 10)
+
+Label(app, text = "Encrypted text:", font = "default 11 bold").grid(row = 0, column = 2, pady = 10)
 final_text = Text(app)
-final_text.pack(padx = 30, pady = 7)
+final_text.config(width = 40, height = 16)
+final_text.grid(row = 1, column = 2, rowspan = 5, padx = 30)
+
 app.mainloop()
